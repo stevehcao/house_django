223f865b6c854e13d2ab1199c676e3b7dbacee02
Steve Cao <steve.cao7@gmail.com>
2020-05-05 20:53:59 -0700

    listing and realtor migration and admin setup

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Listing  # whatever you had in models of this app
 
 # Register your models here.
+admin.site.register(Listing)
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Realtor  # whatever you had in models of this app
+
 # Register your models here.
+admin.site.register(Realtor)
